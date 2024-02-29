a5e5699160f045c695deb08d92586f9ff2807827
Muhammad Faisal Ameen <faisalamin4744@gmail.com>
2024-02-29 10:57:33 +0500

    Added packages and configuration

---
+++ IdentityManagement.API/Data/IdentityManagementDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManagement.API.Data
+{
+    public class IdentityManagementDbContext:IdentityDbContext
+    {
+        public IdentityManagementDbContext(DbContextOptions<IdentityManagementDbContext> options) : base(options)
+
+        {
+
+        }
+    }
+}
--- IdentityManagement.API/Program.cs
+++ IdentityManagement.API/Program.cs
@@ -1,3 +1,9 @@
+using IdentityManagement.API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +12,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// SQL Server connection
+builder.Services.AddDbContext<IdentityManagementDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityManagement")));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    // Configure identity options here.
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<IdentityManagementDbContext>()
+.AddDefaultTokenProviders();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
