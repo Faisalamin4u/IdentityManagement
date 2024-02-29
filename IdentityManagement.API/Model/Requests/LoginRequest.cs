128602e178feebbe2d7bcd6dc17b9f37584fb967
Muhammad Faisal Ameen <faisalamin4744@gmail.com>
2024-02-29 11:08:13 +0500

    Added Models

---
+++ IdentityManagement.API/Model/Requests/LoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityManagement.API.Model.Requests
+{
+    public class LoginRequest
+    {
+        [Required,DataType(DataType.EmailAddress)] 
+        public string Email { get; set; }
+        [Required] 
+        public string Password { get; set; }
+    }
+}
---
+++ IdentityManagement.API/Model/Requests/RegisterRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityManagement.API.Model.Requests
+{
+    public class RegisterRequest
+    {
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        [Required,Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
